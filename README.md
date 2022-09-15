37448056dce12df657788d3c0fe3479d7e201ea3
Gregory Paton <gregpaton08@gmail.com>
2022-09-15 05:48:18 -0400

    adding usage

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
 # Mac Sleep Script
 
 Script to put a Mac to sleep after a given number of minutes.
+
+## Usage
+
+Provide the number of minutes to wait before going to sleep.
+```bash
+./sleep.sh 60
+```
+
+Just run the script and provide the number of minutes to sleep as user input.
+```bash
+./sleep.sh
+How many minutes before going to sleep? 60
+```
+
+## shellcheck
+
+```bash
+brew install shellcheck
+shellcheck sleep.sh
+```
--- sleep.sh
+++ sleep.sh
@@ -1,16 +1,21 @@
 #!/usr/bin/env bash
 
+print_usage () {
+  echo "usage: ${0} [minutes to wait before sleep]"
+}
+
 # Read the number of minutes from the command line.
 MINUTES=${1}
 # If minutes wasn't provided then ask the user.
 if [ -z ${MINUTES} ];  then
-  read -p "Hoe many minutes before going to sleep? " MINUTES
+  read -p "How many minutes before going to sleep? " MINUTES
 fi
 
 # Validate the input is a valid integer.
 re='^[0-9]+$'
 if ! [[ $MINUTES =~ $re ]] ; then
    echo "error: ${MINUTES} is not a number" >&2
+   print_usage
    exit 1
 fi
 
